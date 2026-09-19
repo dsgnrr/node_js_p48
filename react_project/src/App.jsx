8f1eb501c44336f73478b2e433c121326be0b970
Semeniuk Oleg <semeniuk.oleg2002@gmail.com>
2026-09-19 12:57:18 +0300

    temp

--- react_project/src/App.jsx
+++ react_project/src/App.jsx
@@ -5,19 +5,32 @@ const App = () => {
 
   const [counter, setCounter] = useState(0);
 
+  const [numbers, setNumbers] = useState([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
   const framework = 'React';
 
   // let counter = 0;
-
-  const counterIncrement=()=>{
-    setCounter(counter+1);
+  setInterval(() => {
+    if ((Math.floor(Math.random() * 2)) === 0) {
+      setNumbers([]);
+    } else {
+      setNumbers([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+    }
+  }, 2000)
+  const counterIncrement = () => {
+    setCounter(counter + 1);
     console.log(counter)
   }
 
   return (
     <div className="container">
       <h1 className="welcome_title">Welcome to {framework}</h1>
-      <button onClick={()=>setCounter(counter+1)} className="button">Click: {counter}</button>
+      <button onClick={() => setCounter(counter + 1)} className="button">Click: {counter}</button>
+      <ul>
+        {numbers.length == 0 ? (<li>No elements</li>) :
+          numbers.map((v, i) => (
+            <li key={i}>{v}</li>
+          ))}
+      </ul>
     </div>
   )
 }
