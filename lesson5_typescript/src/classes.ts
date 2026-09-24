f9421585665b112a499741b102a7ed85c35cda82
Semeniuk Oleg <semeniuk.oleg2002@gmail.com>
2026-09-24 17:33:22 +0300

    react

--- lesson5_typescript/src/classes.ts
+++ lesson5_typescript/src/classes.ts
@@ -1,14 +1,103 @@
-export class User{
+export class User {
     static readonly company = "Microsoft";
     public id: number;
     protected role: string;
-    private secretToken: string;
+    private _secretToken: string;
 
-    constructor(id:number, role:string, token: string){
+    constructor(id: number, role: string, token: string) {
         this.id = id;
         this.role = role;
-        this.secretToken = token;
+        this._secretToken = token;
+    }
+    public getProfileInfo(): string {
+        return `User ID: ${this.id}, role: ${this.role}`;
+    }
+
+    // private setToken(new_value: string) {
+    //     this.secretToken = new_value;
+    // }
+
+    private set secretToken(value:string){
+        this._secretToken = value;
+    }
+
+    get profile():string{
+        return this.getProfileInfo();
     }
 }
 
-export const userObject:User = new User(1,'Ivan', '21431241241242');
+console.log(User.company);
+export const userObject: User = new User(1, 'Ivan', '21431241241242');
+
+
+class Product {
+    constructor(
+        public readonly id: number,
+        public title: string,
+        public price: number
+    ) { }
+
+    /**
+     * getPrice
+ :number    */
+    public getPrice(): number {
+        return this.price;
+    }
+}
+
+// Inheritance
+
+class Animal {
+    constructor(public name: string) { }
+
+    public makeSound(): void { }
+}
+
+class Dog extends Animal {
+    constructor(name: string, public breed: string) {
+        super(name);
+    }
+
+    public override makeSound(): void {
+        console.log("Bark!");
+    }
+}
+
+// Implimentation
+
+interface Logger {
+    log(message: string): void;
+}
+
+interface Serializable {
+    serialize(): string;
+}
+
+class Sample implements Logger, Serializable {
+    log(message: string): void {
+
+    }
+    serialize(): string {
+        return "";
+    }
+}
+
+// abstract
+
+abstract class BaseRepository<T> {
+    abstract findById(id: number): T;
+
+    abstract selectAll(): T[];
+}
+
+interface UserData { id: number; name: string; }
+
+class UserRepository extends BaseRepository<UserData>{
+    findById(id: number): UserData {
+        return {id:1, name:"Petro"};
+        
+    }
+    selectAll(): UserData[] {
+        return [{id:1, name:"Petro"}];
+    }
+}
--- lesson5_typescript/src/index.ts
+++ lesson5_typescript/src/index.ts
@@ -1,4 +1,5 @@
 import { User, userObject } from "./classes.js";
-
+// userObject.setToken("231");
 console.log(userObject);
 
+console.log(userObject.profile);
